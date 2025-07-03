3ec0ccbc9d8146a6e400a65c3b3dc63fa25c1a1d
deboraj-abybaby <deboraj@abybaby.co.in>
2025-07-03 10:51:06 +0530

    abs

--- src/validators/loginValidator.js
+++ src/validators/loginValidator.js
@@ -1,10 +1,10 @@
-const Joi = require('joi');
+// const Joi = require('joi');
 
-exports.validateLogin = (data) => {
-  const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(6).required()
-  });
+// exports.validateLogin = (data) => {
+//   const schema = Joi.object({
+//     email: Joi.string().email().required(),
+//     password: Joi.string().min(6).required()
+//   });
 
-  return schema.validate(data);
-};
+//   return schema.validate(data);
+// };
